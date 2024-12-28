7516a97ee98599411b371dd82dab4ff549a4624c
Harry Goatman <hg@harrygoatman.com>
2024-12-28 23:20:07 +0000

    feat(7): part 2

--- 7/src/main.rs
+++ 7/src/main.rs
@@ -44,17 +44,22 @@ impl FromStr for Equation {
 
 #[derive(Debug)]
 enum BridgeRepairError {
-    ParseError(ParseEquationError),
+    ParseError {
+        line_number: u32,
+        error: ParseEquationError,
+    },
 }
 
+#[derive(Clone, Copy)]
 enum Operator {
     Add,
     Multiply,
+    Concatenation,
 }
 
 fn load_input() -> String {
     let args: Vec<String> = env::args().collect();
-    fs::read_to_string(args.get(1).unwrap()).expect("Should have been able to read the file")
+    fs::read_to_string(args.get(1).unwrap()).expect("should have been able to read the file")
 }
 
 fn main() -> Result<(), BridgeRepairError> {
@@ -63,32 +68,59 @@ fn main() -> Result<(), BridgeRepairError> {
     let input = load_input();
     let equations = input
         .split('\n')
-        .filter(|line| !line.is_empty())
-        .map(|line| line.parse())
-        .collect::<Result<Vec<Equation>, ParseEquationError>>()
-        .map_err(BridgeRepairError::ParseError)?;
+        .enumerate()
+        .filter(|(_, line)| !line.is_empty())
+        .map(|(line_number, line)| {
+            line.parse().map_err(|err| BridgeRepairError::ParseError {
+                line_number: line_number as u32,
+                error: err,
+            })
+        })
+        .collect::<Result<Vec<Equation>, BridgeRepairError>>()?;
 
     equations.iter().for_each(|e| log::debug!("{:?}", e));
 
-    let possible_equations = equations
-        .iter()
-        .filter(|equation| is_possible(equation))
-        .collect::<Vec<&Equation>>();
+    const PART_1_OPERATORS: [Operator; 2] = [Operator::Add, Operator::Multiply];
+    let total_calibration_result =
+        calculate_total_calibration_result(&PART_1_OPERATORS, &equations);
+    println!("{}", total_calibration_result);
 
-    let total_calibration_result: u64 = possible_equations
-        .iter()
-        .map(|equation| equation.test_value)
-        .sum();
+    const PART_2_OPERATORS: [Operator; 3] =
+        [Operator::Add, Operator::Multiply, Operator::Concatenation];
+    let total_calibration_result =
+        calculate_total_calibration_result(&PART_2_OPERATORS, &equations);
     println!("{}", total_calibration_result);
 
     Ok(())
 }
 
-fn is_possible(equation: &Equation) -> bool {
-    return is_possible_recursive(equation.test_value, None, &equation.numbers);
+fn calculate_total_calibration_result(operators: &[Operator], equations: &[Equation]) -> u64 {
+    let possible_equations = equations
+        .iter()
+        .filter(|equation| is_possible(operators, equation))
+        .collect::<Vec<&Equation>>();
+
+    log::debug!("Num possible: {}", possible_equations.len());
+    equations.iter().for_each(|e| log::debug!("{:?}", e));
+
+    let total_calibration_result: u64 = possible_equations
+        .iter()
+        .map(|equation| equation.test_value)
+        .sum();
+
+    total_calibration_result
 }
 
-fn is_possible_recursive(test_value: u64, current_value: Option<u64>, numbers: &[u64]) -> bool {
+fn is_possible(operators: &[Operator], equation: &Equation) -> bool {
+    return is_possible_recursive(operators, equation.test_value, None, &equation.numbers);
+}
+
+fn is_possible_recursive(
+    operators: &[Operator],
+    test_value: u64,
+    current_value: Option<u64>,
+    numbers: &[u64],
+) -> bool {
     if numbers.is_empty() {
         if current_value == None {
             return false;
@@ -98,21 +130,77 @@ fn is_possible_recursive(test_value: u64, current_value: Option<u64>, numbers: &
     }
 
     if current_value == None {
-        return is_possible_recursive(test_value, Some(numbers[0]), &numbers[1..]);
+        return is_possible_recursive(operators, test_value, Some(numbers[0]), &numbers[1..]);
     }
 
     let current = current_value.unwrap();
+    if current > test_value {
+        return false;
+    }
 
-    const OPERATORS: [Operator; 2] = [Operator::Add, Operator::Multiply];
-    OPERATORS.into_iter().any(|op| {
-        let new_value = apply(op, current, numbers[0]);
-        return is_possible_recursive(test_value, Some(new_value), &numbers[1..]);
+    operators.into_iter().any(|op| {
+        let new_value = apply(*op, current, numbers[0]);
+        if new_value == None {
+            return false;
+        }
+
+        return is_possible_recursive(
+            operators,
+            test_value,
+            Some(new_value.unwrap()),
+            &numbers[1..],
+        );
     })
 }
 
-fn apply(op: Operator, a: u64, b: u64) -> u64 {
+fn apply(op: Operator, a: u64, b: u64) -> Option<u64> {
     match op {
-        Operator::Add => a + b,
-        Operator::Multiply => a * b,
+        Operator::Add => Some(a + b),
+        Operator::Multiply => Some(a * b),
+        Operator::Concatenation => {
+            if a == 0 {
+                return None;
+            }
+
+            let num_digits = b.ilog10() + 1;
+            log::trace!("Num Digits of {}: {}", a, num_digits);
+            const TEN: u64 = 10;
+            a.checked_mul(TEN.checked_pow(num_digits)?)?.checked_add(b)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{apply, is_possible, Equation, Operator};
+
+    #[test]
+    fn test_apply() {
+        let _ = env_logger::try_init();
+        let actual = apply(Operator::Concatenation, 12345, 6789);
+        let expected = Some(123456789);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_concat() {
+        let actual = is_possible(
+            &[Operator::Add, Operator::Multiply, Operator::Concatenation],
+            &Equation {
+                test_value: 123456789101112,
+                numbers: vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            },
+        );
+
+        assert!(actual)
+    }
+
+    #[test]
+    fn test_concat_2() {
+        let actual = apply(Operator::Concatenation, 1, 2);
+        let expected = Some(12);
+
+        assert_eq!(expected, actual);
     }
 }
